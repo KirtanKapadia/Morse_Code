c1320081b22a875eb7b067766999f374e7d33731
Kirtan Kapadia <43675706+KirtanKapadia@users.noreply.github.com>
2019-04-05 22:32:51 +0100

    Add files via upload

--- morse_code.py
+++ morse_code.py
@@ -1,3 +1,6 @@
+import RPi.GPIO as GPIO
+import time
+import string
 
 class Morse_code(object):
 
@@ -10,6 +13,7 @@ class Morse_code(object):
                             "u":"..-", "v":"...-", "w":".--", "x":"-..-", "y":"-.--", "z":"--..", " ":"/",
                             "1":".----", "2":"..---", "3":"...--", "4":"....-", "5":".....", "6":"-....", "7":"--...",
                             "8":"---..", "9":"----.", "0":"-----"}
+        self.PIN = 18
 
     def encode(self,msg):
         msglist = list(msg)
@@ -30,11 +34,30 @@ class Morse_code(object):
                 result += list(self.morse.keys())[list(self.morse.values()).index(element)]
         return result
 
+    def setupPi():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.PIN,GPIO.OUT)
 
-
-
-
-
+    def transmit(self, morse_msg):
+        setupPi()
+        for letter in morse_msg:
+            if letter == ".":
+                GPIO.output(18,GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(18,GPIO.LOW)
+            if letter == "-":
+                GPIO.output(18,GPIO.HIGH)
+                time.sleep(2)
+                GPIO.output(18,GPIO.LOW)
+            if letter == " ":
+                GPIO.output(18,GPIO.HIGH)
+                time.sleep(3)
+                GPIO.output(18,GPIO.LOW)
+            if letter == "/":
+                GPIO.output(18,GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(18,GPIO.LOW)
 
 if "__main__" == __name__:
     m = Morse_code()
